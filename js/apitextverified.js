05f30b07f078581ea6f1f844e5515c83c80bae08
haku1806 <thangtranhtn2000@gmail.com>
2022-10-01 15:55:08 +0700

    Add hotmail panel

--- js/apitextverified.js
+++ js/apitextverified.js
@@ -1,6 +1,7 @@
-var host = "http://server.hakucloud.com:1812";
+var host = "http://server.hakucloud.com:1812/api";
 var api = "";
 var token = "";
+var token_valid = false;
 
 async function check_live_account() {
     api = document.getElementById('api').value;
@@ -36,11 +37,13 @@ function get_token() {
         console.log(result);
         if (result.success) {
             token = result.data.bearer_token;
+            token_valid = true;
             console.log(token);
             noti(0, result.message);
             get_user();
             get_targets();
         } else {
+            token_valid = false;
             console.log(result.message);
             noti(1, result.message);
         }
@@ -49,6 +52,67 @@ function get_token() {
         console.log('error', error)
         noti(1, error);
     });
+}
+
+async function auto_refresh_token() {
+    setInterval(function(){
+        console.log("Refresh Token")
+        if (token_valid == true) {
+            refresh_token();
+        } else {
+            console.log("Token false")
+        }
+       }, 300000);
+}
+
+function refresh_token() {
+    url = host + "/textverified/auth?api=" + api 
+
+    headers = {
+        "Accept": "application/json",
+    }
+
+    params = {}
+    var requestOptions = {
+        method: 'GET',
+        headers: headers,
+        redirect: 'follow',
+    };
+    console.log(requestOptions)
+    fetch(url, requestOptions)
+    .then( response => {
+        if (!response.ok) {
+            noti(1, `HTTP error: ${response.status}`);
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        return response.json();
+    })
+    .then(result => {
+        console.log(result);
+        if (result.success) {
+            token_valid = true;
+            token = result.data.bearer_token;
+            console.log(token);
+            noti(0, result.message);
+        } else {
+            token_valid = false;
+            console.log(result.message);
+            noti(1, result.message);
+        }
+    })
+    .catch(error => {
+        console.log('error', error)
+        noti(1, error);
+    });
+}
+
+async function auto_refresh_user() {
+    setInterval(function(){
+        if (token_valid == true) {
+            get_user();
+        }
+       }, 120000);
 }
 
 function get_user() {
@@ -118,7 +182,7 @@ function get_targets() {
     .then(result => {
         console.log(result);
         if (result.success) {
-            console.log(result.data);
+            // console.log(result.data);
             data = result.data;
             $('#targets').empty();
             $.each(data, function(index, value) {
@@ -224,7 +288,7 @@ function get_detail_phone() {
             $('#phone').attr("data-id", data.id);
             $('#phone').val(data.number);
             if (data.status == "Completed") {
-                $('#code').val(data.number);
+                $('#code').val(data.code);
                 $('#full_message').val(data.sms);
             } else {
                 $('#code').val("");
@@ -233,6 +297,70 @@ function get_detail_phone() {
             
             $('#target_name').text(data.target_name);
             $('#cost').text(data.cost);
+            $('#status').text(data.status);
+            $('#time_remaining').text(data.time_remaining);
+            noti(0, result.message);
+        } else {
+            console.log(result.message);
+            noti(1, result.message);
+        }
+    })
+    .catch(error => {
+        console.log('error', error)
+        noti(1, error);
+    });
+}
+
+
+async function auto_refresh_get_detail_phone() {
+    setInterval(function(){
+        if ($('#status').text() == "Pending") {
+            console.log("Refresh detail phone")
+            refresh_get_detail_phone();
+        }
+       }, 5000);
+}
+
+function refresh_get_detail_phone() {
+
+    id = $('#phone').attr("data-id");
+    service_name = $('#service_name').text();
+    console.log(service_name);
+    url = host + "/textverified/verifications/details?token=" + token + "&id=" + id
+
+    headers = {
+        "Accept": "application/json",
+    }
+
+    params = {}
+    var requestOptions = {
+        method: 'GET',
+        headers: headers,
+        redirect: 'follow',
+    };
+    console.log(requestOptions)
+    fetch(url, requestOptions)
+    .then( response => {
+        if (!response.ok) {
+            noti(1, `HTTP error: ${response.status}`);
+            throw new Error(`HTTP error: ${response.status}`);
+        }
+
+        return response.json();
+    })
+    .then(result => {
+        console.log(result);
+        if (result.success) {
+            console.log(result.data);
+            data = result.data;
+            if (data.status == "Completed") {
+                $('#code').val(data.code);
+                $('#full_message').val(data.sms);
+            } else {
+                $('#code').val("");
+                $('#full_message').val("");
+            }
+            
             $('#status').text(data.status);
             $('#time_remaining').text(data.time_remaining);
             noti(0, result.message);
@@ -352,11 +480,14 @@ function report_phone() {
 }
 /*-------------------------------------------------*/
 $(document).ready(function() {
-    load_data()
+    load_data();
+    auto_refresh_token();
+    auto_refresh_user();
+    auto_refresh_get_detail_phone();
 });
 
 function noti(id, message) {
-    list_notify = ['OK', "Fail"]
+    list_notify = ['OK', "Fail", "Fail"]
     list_notify_status = ['success', 'danger', 'warning']
     $.notify({
         title: '<strong>' + list_notify[id] + '!</strong>',
@@ -373,6 +504,7 @@ function copyClipboard(idInput) {
     var copyText = document.getElementById(idInput);
     copyText.select();
     document.execCommand("copy");
+    noti(0, "Copy successful!")
   }
 
 function saveData() {
---
+++ js/tmail.js
@@ -0,0 +1,229 @@
+var host = "http://localhost:1812/api";
+var account = []
+var message_id = 0
+
+async function check_mail() {
+    var email = $('#email').val()
+
+    // Format input
+    if (validate_email(email) == false) {
+        noti(1, "Invalid Email!");
+    } else {
+        console.log("OK")
+        save_data();
+
+        url = host + "/mail/fetch?mail=" + account[0] + "&pwd=" + account[1] + "&subject=" + $('#subject').val();
+        headers = {
+            "Accept": "application/json",
+        }
+
+        params = {}
+        var requestOptions = {
+            method: 'GET',
+            headers: headers,
+            redirect: 'follow',
+        };
+        console.log(requestOptions)
+        fetch(url, requestOptions)
+            .then(response => {
+                if (!response.ok) {
+                    noti(1, `HTTP error: ${response.status}`);
+                    throw new Error(`HTTP error: ${response.status}`);
+                }
+
+                return response.json();
+            })
+            .then(result => {
+                console.log(result);
+                if (result.success) {
+                    console.log(result.data);
+                    $('#subject_list').empty();
+                    $("#message_content").empty();
+                    for (let i = result.data.messages.length;  i >= 1; i--) {
+                        data = result.data.messages[i-1];
+
+                        // Convert datetime to localtime
+                        var sender_time = new Date(data.datetime);
+                        var localtime = new Date(sender_time.toLocaleString())
+                        var sender_time_local = localtime.getDate().toString() + "/" + localtime.getMonth().toString() + "/" + localtime.getFullYear().toString() + " " + localtime.getHours().toString() + ":" + localtime.getMinutes().toString() +  ":" + localtime.getSeconds().toString();
+                    // for (let i = 1; i <= result.data.messages.length; i++) {
+                    //     data = result.data.messages[i-1];
+                        // console.log(data.sender);
+                        $('#subject_list').append(
+                            '<div id=\"' + i + '\"' +
+                                    'class="w-full p-5 cursor-pointer hover:bg-gray-50" data-id=\"' + i + '\" onclick=\'read_mail_detail(\"' + i + '\")\'>' +
+                                    '<div class="flex justify-between">' + 
+                                        '<div>' +
+                                            '<div class="text-gray-800">' + data.sender + '</div>' +
+                                            '<div class="text-xs text-gray-400">' + data.from + '</div>' +
+                                        '</div>' +
+                                        '<div>' +
+                                            '<div class="text-gray-800 text-sm">' + sender_time_local + '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                    '<div class="text-gray-600 mt-5 text-sm truncate">' + 
+                                        data.subject +
+                                    '</div>' +
+                                '</div>'
+                        )
+
+                        $("#message_content").append(
+                            '<div id="message_' + i + '" class="flex-1 lg:flex flex-col" style="display:none">' +
+            '<textarea class="hidden">To: ' + data.to + 
+' From: ' + data.sender + ' &lt;' + data.from + '&gt;' +
+' Subject: ' + data.subject + 
+' Date: 30 Sep 2022 10:45 AM' + 
+' Content-Type: text/html ' +
+
+data.body + '</textarea>' +
+            '<div class="flex flex-col flex-none py-5 px-6">' +
+                '<div class="flex justify-between items-center">' +
+                    '<div>' +
+                        '<div class="text-gray-900 text-lg">' + data.subject + '</div>' +
+                        '<div class="text-xs text-gray-400">' + data.sender + ' - ' + data.from + '</div>' +
+                    '</div> \
+                    <div> \
+                        <div class="text-xs text-gray-400">' + sender_time_local + '</div> \
+                    </div> \
+                </div> \
+            </div>' +
+            '<iframe class="w-full flex flex-grow px-5" srcdoc=\'' + data.body + '\' frameborder=\'0\'></iframe>' +
+                    '</div>'
+                        )
+                    }
+
+
+                    noti(0, result.message);
+                } else {
+                    console.log(result.message);
+                    noti(1, result.message);
+                }
+            })
+            .catch(error => {
+                console.log('error', error)
+                noti(1, error);
+            });
+    }
+
+
+    // Format input
+
+
+    // url = host + "/mail/fetch?"
+
+    // headers = {
+    //     "Accept": "application/json",
+    // }
+
+    // params = {}
+    // var requestOptions = {
+    //     method: 'GET',
+    //     headers: headers,
+    //     redirect: 'follow',
+    // };
+    // console.log(requestOptions)
+    // fetch(url, requestOptions)
+    // .then( response => {
+    //     if (!response.ok) {
+    //         noti(1, `HTTP error: ${response.status}`);
+    //         throw new Error(`HTTP error: ${response.status}`);
+    //     }
+
+    //     return response.json();
+    // })
+    // .then(result => {
+    //     console.log(result);
+    //     if (result.success) {
+    //         token = result.data.bearer_token;
+    //         token_valid = true;
+    //         console.log(token);
+    //         noti(0, result.message);
+    //         get_user();
+    //         get_targets();
+    //     } else {
+    //         token_valid = false;
+    //         console.log(result.message);
+    //         noti(1, result.message);
+    //     }
+    // })
+    // .catch(error => {
+    //     console.log('error', error)
+    //     noti(1, error);
+    // });
+}
+
+function read_mail_detail(id) {
+    $('#message_' + message_id).hide();
+    $('#message_' + id).show();
+    message_id = id;
+    console.log(message_id);
+    document.querySelector('.message-content').scrollIntoView({ behavior: 'smooth' });
+}
+
+/*-------------------------------------------------*/
+$(document).ready(function () {
+    load_data();
+});
+
+function copyClipboard(idInput) {
+
+    var copyText = document.getElementById(idInput);
+    copyText.select();
+    document.execCommand("copy");
+    noti(0, "Copy successful!")
+}
+
+function validate_email(inputText) {
+    // var email = inputText.replace('\t', ' ').trim();
+    // account = inputText.split(' ');
+    // if (account.length == 1) {
+    //     noti(1, "Email not valid. Please check and try again.");
+    //     return false;
+    // }
+
+    // var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    // if (mailformat.test(account[0])) {
+    //     console.log("Email is valid");
+    //     return true;
+    // } else {
+    //     noti(1, "Email not valid. Please check and try again.");
+    //     return false;
+    // }
+
+    var email = inputText.replace('\t', ' ').trim();
+    account = email.split(' ');
+    if (account.length == 1) {
+        return false;
+    }
+
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (mailformat.test(account[0])) {
+        console.log("Email is valid");
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function load_data() {
+    $('#email').val(localStorage['tmail_email']);
+}
+function save_data() {
+    var email = $('#email').val();
+    if (validate_email(email)) {
+        localStorage.setItem('tmail_email', email);
+    }
+}
+
+function noti(id, message) {
+    list_notify = ['OK', "Fail", "Fail"]
+    list_notify_status = ['success', 'danger', 'warning']
+    $.notify({
+        title: '<strong>' + list_notify[id] + '!</strong>',
+        message: message
+    }, {
+        type: list_notify_status[id],
+        delay: 1,
+        timer: 500
+    });
+}
